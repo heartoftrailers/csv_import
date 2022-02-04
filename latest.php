09d0f5b7961f393c9caa6c4e7349d06e1204dac5
Alex <ciovicaflorin0@gmail.com>
2022-02-04 02:09:27 +0000

    latest.php updated

--- latest.php
+++ latest.php
@@ -4,19 +4,29 @@ $url = "Book1.csv";
 
 $current = file_get_contents($url);
 
-$a = htmlspecialchars($current);
+// $a = htmlspecialchars($current);
+$a = htmlspecialchars_decode($current);
+ 
 
+$b = htmlspecialchars_decode($a);
+ 
+$c =htmlspecialchars_decode($b);
 
-$patterns = array();
-$patterns[1] = '/jpg;http/';
+// $patterns = array();
+// $patterns[1] = '/jpg;http/';
 
-$replacements = array();
-$replacements[0] = 'jpg,http';
+// $replacements = array();
+// $replacements[0] = 'jpg,http';
 
 // $f = html_entity_decode($a, ENT_COMPAT | ENT_HTML5);
-$f= $a;
-$g = preg_replace($patterns, $replacements, $f);
+
+// $g = preg_replace($patterns, $replacements, $a);
+
+file_put_contents('../php training/latest8.csv',$a);
+
+$x = str_getcsv($a);
+
+print_r($x);
 
 
-file_put_contents('../php training/latest7.csv', $g);
 ?>
